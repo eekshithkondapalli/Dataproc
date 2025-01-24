97b4675cd4f107fc00c98853abc326fc8ff9a825
eekshithkondapalli <eekshithkondapalli@gmail.com>
2025-01-24 09:13:55 -0600

    Update spark_write_demo.py

--- spark_write_demo.py
+++ spark_write_demo.py
@@ -3,5 +3,5 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 
 print('Storing random numbers in a hive table')
-spark.range(100).write.mode("overwrite").saveAsTable("random_numbers")
+spark.range(100).write.mode("overwrite").parquet("gs://persuasive-net-447201-h6/random_numbers")
 print('complete')
